89410b084158d6f684da222ea3fee15024839a13
Miles Au <miles.d.au@gmail.com>
2020-03-30 13:30:20 -0400

    added testing for donor controller for creating donors

--- spec/controllers/donors_controller_spec.rb
+++ spec/controllers/donors_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe DonorsController, type: :controller do
+RSpec.describe Api::V1::DonorsController, type: :controller do
+
+  it "creates donor" do
+    post :create, params: {name: "Jack Reacher", status: "ApplicationPending", address: {line_1: "123 Main St", zip_code: "80221", town: "Denver"}}
+    response_body = JSON.parse(response.body)
+    expect(response_body.keys).to eql ["status", "code"]
+    expect(response_body["code"]).to eql 200
+  end
 
 end
